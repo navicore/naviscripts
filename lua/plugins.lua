370d93c96b0115c65df29ad26f118afd3e1d19af
Ed Sweeney <ed@onextent.com>
2023-01-24 12:46:08 -0800

    ok

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,18 +1,10 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-
-  -- Simple plugins can be specified as strings
-  use 'rstacruz/vim-closer'
-
-  -- Lazy loading:
-  -- Load on specific commands
-  use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
 
   use {
     'nvim-tree/nvim-tree.lua',
@@ -20,5 +12,56 @@ return require('packer').startup(function(use)
       'nvim-tree/nvim-web-devicons', -- optional, for file icons
     }
   }
+
+  use 'mattn/calendar-vim'
+  use 'tpope/vim-fugitive'
+  use 'ervandew/supertab'
+  use 'leafgarland/typescript-vim'
+  use 'pangloss/vim-javascript'
+  use 'airblade/vim-gitgutter'
+  use 'ryanoasis/vim-devicons'
+  use 'aklt/plantuml-syntax'
+  use 'chrisbra/csv.vim'
+  use 'christoomey/vim-tmux-navigator'
+  use 'derekwyatt/vim-sbt'
+  use 'derekwyatt/vim-scala'
+  use 'ervandew/screen'
+  use 'fatih/vim-go'
+  use 'godlygeek/tabular'
+
+  use 'mattn/gist-vim'
+  use 'mattn/webapi-vim'
+  use 'navicore/vis.vim'
+  use 'navicore/vissort.vim'
+  use 'rust-lang/rust.vim'
+  use 'tomtom/tcomment_vim'
+  use 'vim-airline/vim-airline'
+  use 'vim-airline/vim-airline-themes'
+  use 'vim-pandoc/vim-pandoc'
+  use 'vim-pandoc/vim-pandoc-syntax'
+  use 'dense-analysis/ale'
+  use 'wannesm/wmgraphviz.vim'
+  use 'diepm/vim-rest-console'
+  use 'brandonbloom/vim-factor'
+
+use {'neoclide/coc.nvim', branch = 'release'}
+use {'jalvesaq/Nvim-R', branch = 'stable' }
+
+-- start telescope
+use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+        ts_update()
+    end,
+}
+use 'sharkdp/fd'
+use 'nvim-lua/plenary.nvim'
+use 'BurntSushi/ripgrep'
+use 'nvim-telescope/telescope-fzf-native.nvim'
+-- end telescope
+
+use 'EdenEast/nightfox.nvim'
+
 
 end)
