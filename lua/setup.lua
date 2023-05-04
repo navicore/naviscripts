09685d2155c2325717f31a1a6e9c458987784acb
Ed Sweeney <ed@onextent.com>
2023-05-03 20:32:57 -0700

    lsp

--- lua/setup.lua
+++ lua/setup.lua
@@ -137,7 +137,44 @@ require("mason").setup({
         },
     }
 })
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup {
+  -- automatically install language servers setup below for lspconfig
+  automatic_installation = true
+}
+
+local nvim_lsp = require('lspconfig')
+nvim_lsp.rust_analyzer.setup{
+  settings = {
+    ['rust-analyzer'] = {
+      diagnostics = {
+        enable = false;
+      }
+    }
+  }
+}
+nvim_lsp.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+nvim_lsp.jedi_language_server.setup{}
 
 
 -- LSP Diagnostics Options Setup 
