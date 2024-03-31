40e111c5c1419243cf57d0a783efcf587ad1dc8f
Ed Sweeney <ed@onextent.com>
2024-03-30 23:46:20 -0700

    more

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,7 +16,7 @@ return require('packer').startup(function(use)
   use {
     'nvim-tree/nvim-tree.lua',
     requires = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+      'nvim-tree/nvim-web-devicons',
     }
   }
 
