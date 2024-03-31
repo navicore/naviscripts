ef40cc13b85183ba8e2126feb6703e9f3ccd76fa
Ed Sweeney <ed@onextent.com>
2024-03-30 18:58:59 -0700

    rust struct and better lua example

--- lua/snippets/lua_snippets.lua
+++ lua/snippets/lua_snippets.lua
@@ -1,7 +1,36 @@
 local ls = require("luasnip")
+local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
 local s = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
 
 ls.add_snippets("lua", {
-    s("trigger", t("Your snippet content here")),
+    s("mySecondSnippet",
+    fmt(
+[[
+local {} = function({})
+    {} {{ I am in curly braces }}
+end
+
+]],
+        {
+            i(1, "myVar"),
+            i(2, "myArg"),
+            i(3, "-- TODO: something"),
+        }
+    )
+    )
+})
+
+ls.add_snippets("rust", {
+  s({ trig = 'struct', dscr = 'Classic `struct` that implements `std::fmt::Debug`' }, {
+    t { '#[derive(Eq, PartialEq, Clone, Debug)]', '' },
+    t { 'struct ' },
+    i(1, 'Name'),
+    t { ' {', '\t' },
+    i(0),
+    t { '', '' },
+    t { '}', '' },
+  }),
 })
