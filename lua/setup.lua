ee48103022651b0d9a0067c13f5f27f3425386c6
Ed Sweeney <ed@onextent.com>
2024-04-03 17:43:20 -0700

    no more vimrc

--- lua/setup.lua
+++ lua/setup.lua
@@ -1,3 +1,87 @@
+-- begin legacy config
+
+vim.o.syntax = "on"
+vim.o.nu = true
+vim.wo.foldenable = false
+vim.filetype.off = true
+vim.o.directory = vim.fn.expand("~/.vim/swapfiles")
+vim.o.undodir = vim.fn.expand("~/.undodir/")
+vim.o.hidden = true
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.g.csv_nomap_bs = 1
+vim.g.csv_nomap_cr = 1
+vim.g.csv_nomap_space = 1
+vim.filetype.plugin = "on"
+vim.filetype.indent = "on"
+vim.o.completeopt = "menuone,noselect"
+vim.g.airline_extensions_ale_enabled = 1
+vim.g.WMGraphviz_dot = 'dot'
+vim.g.WMGraphviz_output = 'png'
+vim.g.WMGraphviz_viewer = 'open'
+vim.o.autoread = true
+vim.o.statusline = "%#warningmsg#"
+vim.o.statusline = "%*"
+vim.g.ScreenImpl = 'Tmux'
+vim.g.vrc_output_buffer_name = '__VRC_OUTPUT.json'
+vim.g.vrc_curl_opts = {
+    ['--connect-timeout'] = 5,
+    ['--max-time'] = 60,
+    ['--ipv4'] = '',
+    ['-ks'] = '',
+}
+vim.o.undofile = true
+vim.g.rustfmt_autosave = 1
+vim.g.pandoc_modules_disabled = {"folding"}
+vim.g.pandoc_syntax_conceal_use = 0
+vim.g.R_assign = 0
+vim.o.textwidth = 80
+if vim.fn.has('unix') and not vim.fn.has('macunix') then
+    vim.g.loaded_clipboard_provider = 1
+end
+vim.o.clipboard = "unnamedplus"
+vim.o.encoding = "utf8"
+vim.g.airline_powerline_fonts = 1
+vim.o.guifont = "Fira Code:h12"
+vim.g.mapleader = ","
+
+-- Mappings
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<F4>', ':w<CR>:make<CR>:copen<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>f', ':ALEFix<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<CR>', ':nohlsearch<CR><CR>', {noremap = true})
+vim.api.nvim_set_keymap('x', '.', ':normal .<CR>', {noremap = true})
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>`^', {noremap = true})
+
+-- Autocommands
+vim.api.nvim_create_autocmd("VimEnter", {
+    pattern = "*",
+    callback = function()
+        if vim.fn.argc() == 0 then
+            vim.cmd('NvimTreeOpen')
+        end
+    end
+})
+
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+    pattern = "*.sbt",
+    callback = function()
+        vim.bo.filetype = "sbt"
+    end
+})
+
+-- Load .vimrc.local if exists
+if vim.fn.filereadable(vim.fn.glob('./.vimrc.local')) == 1 then
+    vim.cmd('source ./.vimrc.local')
+end
+
+-- end legacy config
+
 vim.g['conjure#extract#tree_sitter#enabled'] = true
 vim.g['conjure#filetype#rust'] = false
 vim.o.wrap = false
@@ -335,11 +419,8 @@ autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 
 vim.g.haskell_tools = {
   hls = {
-    ---@param ht is a copy of the haskell-tools module
-    on_attach = function(client, bufnr, ht)
-      local opts = vim.tbl_extend('keep', def_opts, { buffer = bufnr, })
-      -- haskell-language-server relies heavily on codeLenses,
-      -- so auto-refresh (see advanced configuration) is enabled by default
+    on_attach = function(_, _bufnr, ht)
+      local opts = vim.tbl_extend('keep', def_opts, { buffer = _bufnr, })
       vim.keymap.set('n', '<space>ca', vim.lsp.codelens.run, opts)
       vim.keymap.set('n', '<space>hs', ht.hoogle.hoogle_signature, opts)
       vim.keymap.set('n', '<space>ea', ht.lsp.buf_eval_all, opts)
