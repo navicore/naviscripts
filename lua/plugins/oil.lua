fcd6967bebf10979390a3c0e9da69035098c2a0e
Ed Sweeney <ed@onextent.com>
2024-04-07 13:27:41 -0700

    more lazy

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,12 +2,24 @@ return {
   {
     'stevearc/oil.nvim',
     opts = {},
-    -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = {
-      function()
+    config = function()
         require("oil").setup()
-      end
-    },
+
+        -- Set up the key mapping for Oil
+        vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+        -- Create the autocommand within the plugin's configuration
+        vim.api.nvim_create_autocmd("VimEnter", {
+            pattern = "*",
+            callback = function()
+                if vim.fn.argc() == 0 then
+                    vim.defer_fn(function()
+                        vim.cmd('Oil')
+                    end, 0)
+                end
+            end
+        })
+    end,
   },
 }
