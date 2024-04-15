05620425b6077492232793b08929a801f082be84
Ed Sweeney <ed@onextent.com>
2024-04-14 20:03:24 -0700

    cp

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,7 +3,11 @@ return {
     'github/copilot.vim',
     config = function()
         vim.keymap.set('i', '<C-L>', '<Plug>(copilot-accept-word)')
-        vim.cmd('Copilot disable') -- Disable copilot by default and enable it when needed.
+        --vim.cmd('Copilot disable') -- Disable copilot by default and enable it when needed.
+        vim.g.copilot_filetypes = {
+            ['*'] = true,
+            ['markdown'] = false
+        }
     end
   },
 }
