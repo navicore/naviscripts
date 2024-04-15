77f3330991663d73b9280fa424c701b57e4e9d56
Ed Sweeney <ed@onextent.com>
2024-04-14 19:31:13 -0700

    cp

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,6 +3,14 @@ return {
     'github/copilot.vim',
     config = function()
         vim.keymap.set('i', '<C-L>', '<Plug>(copilot-accept-word)')
+        vim.g.copilot_filetypes = {
+            ['*'] = true,
+            ['markdown'] = false,
+            -- ['python'] = true
+            -- ['java'] = true
+            -- ['lua'] = true
+            -- ['rust'] = true
+        }
     end
   },
 }
