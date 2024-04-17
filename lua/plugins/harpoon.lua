785053611e578299ee759b193f217be53cb2fafe
Ed Sweeney <ed@onextent.com>
2024-04-17 05:42:11 -0700

    harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,36 @@
+return {
+  {
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
+      config = function()
+        local harpoon = require('harpoon')
+        harpoon:setup({})
+
+        -- basic telescope configuration
+        local conf = require("telescope.config").values
+        local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+                table.insert(file_paths, item.value)
+            end
+
+            require("telescope.pickers").new({}, {
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table({
+                    results = file_paths,
+                }),
+                previewer = conf.file_previewer({}),
+                sorter = conf.generic_sorter({}),
+            }):find()
+        end
+
+        vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        -- vim.keymap.set("n", "<leader-h>", function() toggle_telescope(harpoon:list()) end,
+        --     { desc = "Open harpoon window" })
+        vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+        { desc = "Open harpoon window" })
+    end,
+  }
+}
