5d501b83c0f1ad8aee797c4e0df288c6d6ac9af5
Ed Sweeney <ed@onextent.com>
2024-04-20 19:57:53 -0700

    term

---
+++ lua/plugins/term.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = function ()
+      require("toggleterm").setup {
+        direction = "horizontal",
+        --direction = "float",
+        -- use leader t to toggle terminal
+        open_mapping = [[<leader>t]],
+        shade_filetypes = {},
+        shade_terminals = true,
+        shade_factor = 1,
+      }
+    end
+  },
+}
