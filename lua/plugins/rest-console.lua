9d54b62b512031ce4f57da0dbc5634af08fcb313
Ed Sweeney <ed@onextent.com>
2024-05-04 06:21:30 -0700

    rest plugin can not be lazy

--- lua/plugins/rest-console.lua
+++ lua/plugins/rest-console.lua
@@ -1,7 +1,7 @@
 return {
   {
     'diepm/vim-rest-console',
-    event = "VeryLazy",
+    --event = "VeryLazy",
     config = function()
       vim.g.vrc_output_buffer_name = '__VRC_OUTPUT.json'
       vim.g.vrc_curl_opts = {
