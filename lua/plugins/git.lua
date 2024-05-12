fc9b3cf3437ea0cbd195879dc726eeb14d88119c
Ed Sweeney <ed@onextent.com>
2024-05-11 19:22:58 -0700

    noocto

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -18,19 +18,4 @@ return {
        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
   },
-  {
-    'pwntester/octo.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope.nvim',
-      'nvim-tree/nvim-web-devicons',
-    },
-    config = function ()
-      require"octo".setup({
-        suppress_missing_scope = {
-          projects_v2 = true,
-        }
-      })
-    end
-  },
 }
