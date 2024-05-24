3f6f7e44953713b468dd8ca8d0d3491c53bacb6b
Ed Sweeney <ed@onextent.com>
2024-05-24 12:53:21 -0700

    prompt

--- wezterm.lua
+++ wezterm.lua
@@ -14,6 +14,7 @@ config.color_scheme = 'Solarized (dark) (terminal.sexy)'
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.9
 config.font_size = 16.0
+config.font = wezterm.font 'JetBrains Mono'
 
 -- and finally, return the configuration to wezterm
 return config
