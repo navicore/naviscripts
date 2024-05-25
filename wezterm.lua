0d6039176aa1aa8059c9f3dbe606c9ca2de1bd23
Ed Sweeney <ed@onextent.com>
2024-05-24 20:48:54 -0700

    solar

--- wezterm.lua
+++ wezterm.lua
@@ -1,14 +1,7 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
--- config.color_scheme = 'AdventureTime'
---config.color_scheme = 'Nancy (terminal.sexy)'
 config.color_scheme = 'Solarized (dark) (terminal.sexy)'
 
 config.hide_tab_bar_if_only_one_tab = true
@@ -16,5 +9,4 @@ config.window_background_opacity = 0.9
 config.font_size = 16.0
 config.font = wezterm.font 'JetBrains Mono'
 
--- and finally, return the configuration to wezterm
 return config
