41756b824ee0422c6e71831357066d8f3cbd563f
Ed Sweeney <ed@onextent.com>
2024-05-24 21:00:42 -0700

    color

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Solarized (dark) (terminal.sexy)'
+--config.color_scheme = 'Solarized (dark) (terminal.sexy)'
+config.color_scheme = 'Relaxed'
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.9
