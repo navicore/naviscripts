908301cd3550e310ac9e271ee97287a700cb62f1
Ed Sweeney <ed@onextent.com>
2024-05-26 06:07:47 -0700

    wezterm resize

--- wezterm.lua
+++ wezterm.lua
@@ -10,4 +10,12 @@ config.window_background_opacity = 0.9
 config.font_size = 16.0
 config.font = wezterm.font 'JetBrains Mono'
 
+config.keys = {
+    -- Resize pane mappings
+    {key="DownArrow", mods="CTRL|SHIFT", action=wezterm.action{SendString="\x1b[1;6B"}},
+    {key="UpArrow", mods="CTRL|SHIFT", action=wezterm.action{SendString="\x1b[1;6A"}},
+    {key="RightArrow", mods="CTRL|SHIFT", action=wezterm.action{SendString="\x1b[1;6C"}},
+    {key="LeftArrow", mods="CTRL|SHIFT", action=wezterm.action{SendString="\x1b[1;6D"}},
+}
+
 return config
