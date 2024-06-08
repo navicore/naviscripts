e1fc09162da6e556d1f9225ab5c4574da2a4d72a
Ed Sweeney <ed@onextent.com>
2024-06-08 09:48:44 -0700

    gd works

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -26,6 +26,13 @@ return {
         server = {
           on_attach = function(client, bufnr)
             require("lsp-inlayhints").on_attach(client, bufnr)
+
+            local opts = { noremap=true, silent=true }
+            local buf_set_keymap = vim.api.nvim_buf_set_keymap
+            buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+            buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+            buf_set_keymap(bufnr, 'n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+            buf_set_keymap(bufnr, 'n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
           end
         }
       }
