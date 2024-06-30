345c76d55732cc75f7b0d1b4ccf042f563822b1d
Ed Sweeney <ed@onextent.com>
2024-06-30 07:29:22 -0700

    note

--- lua/plugins/folds.lua
+++ lua/plugins/folds.lua
@@ -10,6 +10,7 @@ return {
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
 
+        -- reminder, use zr to fold and zm to unfold current scope
         -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
         vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
         vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
