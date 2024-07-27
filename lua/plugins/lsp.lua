bb4fe3f3434f4f04db644a2f4e585c0fc29e927c
Ed Sweeney <ed@onextent.com>
2024-07-27 11:47:23 -0700

    go

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,6 +22,7 @@ return {
       require("mason-lspconfig").setup {
         automatic_installation = true,
         ensure_installed = {
+          "gopls",
           "java_language_server",
           "jedi_language_server",
           "lua_ls",
@@ -74,4 +75,45 @@ return {
   },
 
   {'lvimuser/lsp-inlayhints.nvim'},
+  {'neovim/nvim-lspconfig',
+        config = function()
+            local lspconfig = require('lspconfig')
+
+            -- Configure gopls
+            lspconfig.gopls.setup{
+                on_attach = function(client, bufnr)
+                    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+                    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+                    -- Enable completion triggered by <c-x><c-o>
+                    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+                    -- Mappings.
+                    local opts = { noremap=true, silent=true }
+
+                    -- See `:help vim.lsp.*` for documentation on any of the below functions
+                    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+                    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+                    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+                    buf_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+                    buf_set_keymap('n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+                    buf_set_keymap('n', '<space>wa', '<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+                    buf_set_keymap('n', '<space>wr', '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+                    buf_set_keymap('n', '<space>wl', '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+                    buf_set_keymap('n', '<space>D', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+                    buf_set_keymap('n', '<space>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
+                    buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+                    buf_set_keymap('n', '<space>e', '<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+                    buf_set_keymap('n', '[d', '<Cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+                    buf_set_keymap('n', ']d', '<Cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+                    buf_set_keymap('n', '<space>q', '<Cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+                    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+                end,
+
+                flags = {
+                    debounce_text_changes = 150,
+                }
+            }
+        end,
+    },
 }
