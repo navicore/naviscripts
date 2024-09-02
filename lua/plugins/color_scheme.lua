bf16b2b76748aac1a5252af3f3a87febe54c223f
Ed Sweeney <ed@onextent.com>
2024-09-01 18:43:17 -0700

    case sensitive nordfox

--- lua/plugins/color_scheme.lua
+++ lua/plugins/color_scheme.lua
@@ -18,7 +18,7 @@ return {
       lazy = false,
       priority = 1000,
       config = function()
-        vim.cmd.colorscheme('Nordfox')
+        vim.cmd.colorscheme('nordfox')
         --vim.cmd.colorscheme('Nightfox')
       end
     },
