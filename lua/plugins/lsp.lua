9dce66937be92458fd3f23e636ee0695135f2a68
Ed Sweeney <ed@onextent.com>
2024-09-14 07:58:16 -0700

    go

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,28 +2,6 @@ return {
 
   {
     'neovim/nvim-lspconfig',
-    config = function()
-      local lspconfig = require('lspconfig')
-      lspconfig.gopls.setup{
-        cmd = {"gopls"},
-        filetypes = {"go", "gomod", "gowork", "gotmpl"},
-        root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
-        settings = {
-          gopls = {
-            analyses = {
-              unusedparams = true,
-            },
-            staticcheck = true,
-          },
-        },
-      }
-      local opts = { noremap=true, silent=true }
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    end
   },
 
   {'nvimdev/lspsaga.nvim',
