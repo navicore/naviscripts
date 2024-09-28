7e84c5d92df27832d564724b8bb069987de00e02
Ed Sweeney <ed@onextent.com>
2024-09-28 06:10:04 -0700

    zetrev

--- lua/plugins/useless.lua
+++ lua/plugins/useless.lua
@@ -1,3 +1,10 @@
 return {
-  'eandrju/cellular-automaton.nvim'
+  {
+    'eandrju/cellular-automaton.nvim',
+    config = function()
+      vim.keymap.set("n", "<leader>cr", "<cmd>CellularAutomaton make_it_rain<CR>")
+      vim.keymap.set("n", "<leader>cs", "<cmd>CellularAutomaton scramble<CR>")
+      vim.keymap.set("n", "<leader>cg", "<cmd>CellularAutomaton game_of_life<CR>")
+    end
+  }
 }
