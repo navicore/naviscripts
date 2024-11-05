70e7010d8fb8d908ce321ca7c8b5461e087b5c21
Ed Sweeney <ed@onextent.com>
2024-11-05 09:07:40 -0800

    no hidden again

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,7 +7,7 @@ return {
         require("oil").setup({
             default_file_explorer = true,
             view_options = {
-                show_hidden = true,
+                show_hidden = false,
             }
         })
 
