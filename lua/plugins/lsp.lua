2f82d5690f9bf9ad426b9d43d910e092fadf9856
Ed Sweeney <ed@onextent.com>
2024-12-08 12:28:45 -0800

    no null-ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,29 +1,39 @@
 return {
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-tree/nvim-web-devicons',
-      'williamboman/mason.nvim',
-    },
-    config = function()
-    end
-  },
-
-  {'nvimdev/lspsaga.nvim',
-    config = function()
-      require('lspsaga').setup({
-        ui = {
-            code_action = ''
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+            'nvim-tree/nvim-web-devicons',
+            'williamboman/mason.nvim',
         },
-      })
-    end,
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-tree/nvim-web-devicons'
-    }
-  },
+        config = function()
+            require('lspconfig').lua_ls.setup {
+                settings = {
+                    Lua = {
+                        format = {
+                            enable = false,
+                        },
+                    },
+                },
+            }
+        end
+    },
 
-  {'lvimuser/lsp-inlayhints.nvim'},
+    {
+        'nvimdev/lspsaga.nvim',
+        config = function()
+            require('lspsaga').setup({
+                ui = {
+                    code_action = ''
+                },
+            })
+        end,
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+            'nvim-tree/nvim-web-devicons'
+        }
+    },
+
+    { 'lvimuser/lsp-inlayhints.nvim' },
 
 }
