bfddaa169a671eb9e09f9d0cf6fb6ef396445cf4
Ed Sweeney <ed@onextent.com>
2024-12-08 11:03:13 -0800

    refactor mason and null-ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,4 @@
 return {
-
   {
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -27,41 +26,4 @@ return {
 
   {'lvimuser/lsp-inlayhints.nvim'},
 
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-      local mr = require("mason-registry")
-      local function ensure_installed(package)
-        if not mr.is_installed(package) then
-          mr.get_package(package):install()
-        end
-      end
-      local servers = {
-        "pyright",
-        "gopls",
-        "lua-language-server",  -- This is the correct name for Mason
-        "jedi-language-server",
-        "black"
-      }
-      for _, server in ipairs(servers) do
-        ensure_installed(server)
-      end
-    end
-  },
-
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.black,
-        },
-      })
-    end
-  },
-
 }
-
-
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    -- dependencies = {
+    --   'neovim/nvim-lspconfig',
+    -- },
+    config = function()
+      require("mason").setup()
+      local mr = require("mason-registry")
+      local function ensure_installed(package)
+        if not mr.is_installed(package) then
+          mr.get_package(package):install()
+        end
+      end
+      local servers = {
+        "pyright",
+        "gopls",
+        "lua-language-server",  -- This is the correct name for Mason
+        "jedi-language-server",
+        "black"
+      }
+      for _, server in ipairs(servers) do
+        ensure_installed(server)
+      end
+    end
+  },
+}
