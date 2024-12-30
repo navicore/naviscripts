551895e91ff89c42867b2e6d72d9d146260e92fe
Ed Sweeney <ed@onextent.com>
2024-12-29 20:55:44 -0800

    folds

--- lua/plugins/folds.lua
+++ lua/plugins/folds.lua
@@ -12,8 +12,8 @@ return {
 
         -- reminder, use za to toggle fold and zA to toggle recursively
         -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+        -- vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+        -- vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities.textDocument.foldingRange = {
