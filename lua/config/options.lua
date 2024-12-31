e161f4b8f73c72072af330f248cf299f19b89504
Ed Sweeney <ed@onextent.com>
2024-12-31 15:26:43 -0800

    refactor config to be closer to lazyvim

---
+++ lua/config/options.lua
@@ -0,0 +1,50 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- TODO these are NOT loaded automatically as we are not using lazyvim plugin configs -- see the file above for an example of how to load lazy
+
+vim.o.syntax = "on"
+vim.g.mapleader = ","
+vim.g.maplocalleader = "\\"
+
+vim.opt.spelllang = "en_us"
+vim.opt.spell = true
+
+vim.o.nu = true
+vim.wo.foldenable = false
+vim.filetype.off = true
+vim.o.directory = vim.fn.expand("~/.vim/swapfiles")
+vim.o.undodir = vim.fn.expand("~/.undodir/")
+vim.o.hidden = true
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.g.csv_nomap_bs = 1
+vim.g.csv_nomap_cr = 1
+vim.g.csv_nomap_space = 1
+vim.filetype.plugin = "on"
+vim.filetype.indent = "on"
+vim.o.completeopt = "menuone,noselect"
+vim.g.WMGraphviz_dot = "dot"
+vim.g.WMGraphviz_output = "png"
+vim.g.WMGraphviz_viewer = "open"
+vim.o.autoread = true
+vim.o.statusline = "%#warningmsg#"
+vim.o.statusline = "%*"
+vim.g.ScreenImpl = "Tmux"
+
+vim.o.undofile = true
+vim.g.rustfmt_autosave = 1
+vim.g.pandoc_modules_disabled = { "folding" }
+vim.g.pandoc_syntax_conceal_use = 0
+vim.g.R_assign = 0
+vim.o.textwidth = 80
+if vim.fn.has("unix") and not vim.fn.has("macunix") then
+  vim.g.loaded_clipboard_provider = 1
+end
+
+vim.o.clipboard = "unnamedplus"
+vim.o.encoding = "utf8"
+vim.g.airline_powerline_fonts = 1
+vim.o.guifont = "Fira Code:h12"
---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    on_attach = function(buffer)
+      -- LSP Diagnostics Options Setup
+      local sign = function(opts)
+        vim.fn.sign_define(opts.name, {
+          texthl = opts.name,
+          text = opts.text,
+          numhl = "",
+        })
+      end
+
+      sign({ name = "DiagnosticSignError", text = "" })
+      sign({ name = "DiagnosticSignWarn", text = "" })
+      sign({ name = "DiagnosticSignHint", text = "" })
+      sign({ name = "DiagnosticSignInfo", text = "" })
+
+      vim.diagnostic.config({
+        virtual_text = false,
+        signs = true,
+        update_in_insert = true,
+        underline = true,
+        severity_sort = false,
+        float = {
+          border = "rounded",
+          source = "always",
+          header = "",
+          prefix = "",
+        },
+      })
+
+      vim.cmd([[
+    set signcolumn=yes
+    autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+    ]])
+
+      vim.opt.completeopt = { "menuone", "noselect", "noinsert" }
+      vim.opt.shortmess = vim.opt.shortmess + { c = true }
+      vim.api.nvim_set_option("updatetime", 300)
+
+      vim.cmd([[
+    set signcolumn=yes
+    autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+    ]])
+    end,
+  },
+}
