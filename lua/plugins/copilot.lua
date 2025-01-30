db1eb17db674475fa8819339f0b4f6c2e3c4ef6e
Ed Sweeney <ed@onextent.com>
2025-01-30 05:46:56 -0800

    no chatgpt

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -67,32 +67,5 @@ return {
     dependencies = {
       "zbirenbaum/copilot.lua",
     },
-  },
-  {
-    "jackMort/ChatGPT.nvim",
-    cond = function()
-      return vim.env.OPENAI_API_KEY ~= nil
-    end,
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup({
-        openai_params = {
-          model = "gpt-4o",
-          frequency_penalty = 0,
-          presence_penalty = 0,
-          max_tokens = 4095,
-          temperature = 0.2,
-          top_p = 0.1,
-          n = 1,
-        }
-      })
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "folke/trouble.nvim",
-      --"nvim-telescope/telescope-fzf-native.nvim",
-      "nvim-telescope/telescope.nvim",
-    }
   }
 }
