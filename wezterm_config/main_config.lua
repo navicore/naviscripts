f0f3fba4848eed6ba1a5385667ba198f51e8ddf5
Ed Sweeney <ed@onextent.com>
2025-02-19 06:55:01 -0800

    wezterm

--- wezterm_config/main_config.lua
+++ wezterm_config/main_config.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.window_close_confirmation = "NeverPrompt"
 config.hide_tab_bar_if_only_one_tab = true
 
 -- Function to merge tables
