463ff2eaa87bd394945d77f81c75cd13a7a1b7a9
Ed Sweeney <ed@onextent.com>
2025-02-20 21:49:59 -0800

    nocrates

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -43,22 +43,4 @@ return {
     'mfussenegger/nvim-dap',
     event = "VeryLazy",
   },
-  {
-    'saecki/crates.nvim',
-    tag = 'stable',
-    event = { "BufRead Cargo.toml" },
-    config = function()
-      require('crates').setup({
-        lsp = {
-            enabled = true,
-            on_attach = function(client, bufnr)
-                -- the same on_attach function as for your other lsp's
-            end,
-            actions = true,
-            completion = true,
-            hover = true,
-        },
-      })
-    end,
-  },
 }
