7c94509f842a932ac292b4b71daa96659c1205eb
Ed Sweeney <ed@onextent.com>
2025-03-07 19:37:05 -0800

    copilotchat picker

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,6 +4,7 @@ return {
     version = '*',
     config = function ()
       require("mini.ai").setup { }
+      vim.ui.select = require('mini.pick').ui_select
       -- require("mini.surround").setup { } -- s conflicts with search
       -- require("mini.operators").setup { }
       -- require("mini.pairs").setup { }
