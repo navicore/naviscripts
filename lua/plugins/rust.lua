b750f8d32ae73556dc01d2c7d59813ff62e70869
Ed Sweeney <ed@onextent.com>
2025-03-28 05:38:27 -0700

    error display and ts

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -23,26 +23,14 @@ return {
           },
         },
         server = {
-          -- on_attach = function(client, bufnr)
-          --   require("lsp-inlayhints").on_attach(client, bufnr)
-          --
-          --   local opts = { noremap=true, silent=true }
-          --   local buf_set_keymap = vim.api.nvim_buf_set_keymap
-          --   buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-          --   buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-          --   buf_set_keymap(bufnr, 'n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-          --   buf_set_keymap(bufnr, 'n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
-          -- end
           on_attach = function(client, bufnr)
             require("lsp-inlayhints").on_attach(client, bufnr)
-
             local opts = { noremap=true, silent=true }
             local buf_set_keymap = vim.api.nvim_buf_set_keymap
             buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
             buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
             buf_set_keymap(bufnr, 'n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
             buf_set_keymap(bufnr, 'n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
-            -- buf_set_keymap(bufnr, 'n', '<leader>e', '<Cmd>lua vim.diagnostic.open_float()<CR>', opts) -- Show diagnostics in a floating window
             buf_set_keymap(bufnr, 'n', '<leader>e', '<Cmd>lua vim.diagnostic.open_float(nil, { border = "rounded" })<CR>', opts) -- Floating window with rounded borders
             buf_set_keymap(bufnr, 'n', '<leader>q', '<Cmd>Telescope diagnostics<CR>', opts) -- Show diagnostics in a Telescope window
           end
