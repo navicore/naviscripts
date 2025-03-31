1711e08dba5bf3402b8645acd452c9dea48ff618
Ed Sweeney <ed@onextent.com>
2025-03-31 15:35:44 -0700

    format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -7,6 +7,7 @@ return {
         lua = { "stylua" },
         -- You can customize some of the format options for the filetype (:help conform.format)
         rust = { "rustfmt", lsp_format = "fallback" },
+        go = { "goimports_reviser", "gofmt" },
       },
       format_on_save = {
         -- These options will be passed to conform.format()
