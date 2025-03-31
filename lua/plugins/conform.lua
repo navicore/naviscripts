bc2c1eb64f64413b085a2120048184865f843081
Ed Sweeney <ed@onextent.com>
2025-03-31 15:32:10 -0700

    conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,8 +5,6 @@ return {
     require("conform").setup({
       formatters_by_ft = {
         lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        python = { "black" },
         -- You can customize some of the format options for the filetype (:help conform.format)
         rust = { "rustfmt", lsp_format = "fallback" },
       },
