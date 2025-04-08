64205479f7c2a6d8fcec27096658a98ecfd6dd48
Ed Sweeney <ed@onextent.com>
2025-04-07 18:30:46 -0700

    mini move

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,15 +1,16 @@
 return {
   {
-    'echasnovski/mini.nvim',
-    version = '*',
-    config = function ()
-      require("mini.ai").setup { }
-      vim.ui.select = require('mini.pick').ui_select
+    "echasnovski/mini.nvim",
+    version = "*",
+    config = function()
+      require("mini.ai").setup({})
+      vim.ui.select = require("mini.pick").ui_select
+      require("mini.move").setup({})
       -- require("mini.surround").setup { } -- s conflicts with search
       -- require("mini.operators").setup { }
       -- require("mini.pairs").setup { }
       -- require("mini.bracketed").setup { }
       -- require("mini.files").setup { }
-    end
+    end,
   },
 }
