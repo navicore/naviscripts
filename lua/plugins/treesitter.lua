50e4dd1f5bb7607a297c34cbd82124cd1a5d8e3e
Ed Sweeney <ed@onextent.com>
2025-04-24 16:36:09 -0700

    symbols

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -87,4 +87,18 @@ return {
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     },
   },
+  {
+    "oskarrrrrrr/symbols.nvim",
+    config = function()
+      local r = require("symbols.recipes")
+      require("symbols").setup(r.DefaultFilters, r.AsciiSymbols, {
+        sidebar = {
+          -- custom settings here
+          -- e.g. hide_cursor = false
+        },
+      })
+      vim.keymap.set("n", ",s", "<cmd>Symbols<CR>")
+      vim.keymap.set("n", ",S", "<cmd>SymbolsClose<CR>")
+    end,
+  },
 }
