526a96aff5e43a3fc7b066e75867e8e41d4272ff
Ed Sweeney <ed@onextent.com>
2025-05-03 08:11:10 -0700

    format

--- lua/plugins/naviscripts.lua
+++ lua/plugins/naviscripts.lua
@@ -2,7 +2,22 @@ return {
   {
     "navicore/nvim-naviscripts",
     config = function()
-      require("naviscripts")
+      require("naviscripts").setup()
     end,
+    cmd = "FormatRustFiles", -- Lazy-load on this command
   },
 }
+
+-- {
+--     'navicore/nvim-reminders',
+--     config = function()
+--       -- require('reminders').setup({
+--       --   paths = { '~/git/navicore/zet' },
+--       --   --recursive_scan = true,
+--       -- })
+--       require('reminders').setup()
+--       vim.keymap.set("n", "<leader>zr", "<cmd>ReminderScan<CR>")
+--       vim.keymap.set("n", "<leader>zre", "<cmd>ReminderEdit<CR>")
+--     end
+--   },
+--
