fff88ba5a30b877473f0da8f71dd1800f75bc508
Ed Sweeney <ed@onextent.com>
2025-08-22 05:18:15 -0700

    image

--- lua/plugins/telekasten.lua
+++ lua/plugins/telekasten.lua
@@ -1,26 +1,32 @@
 return {
   {
-    'renerocksai/calendar-vim',
+    "renerocksai/calendar-vim",
   },
   {
-    'renerocksai/telekasten.nvim',
+    "navicore/telekasten.nvim",
     dependencies = {
-      'MeanderingProgrammer/render-markdown.nvim',
-      'nvim-telescope/telescope-fzf-native.nvim',
-      'nvim-telescope/telescope.nvim',
-      'renerocksai/calendar-vim',
+      "MeanderingProgrammer/render-markdown.nvim",
+      "nvim-telescope/telescope-fzf-native.nvim",
+      "nvim-telescope/telescope.nvim",
+      "renerocksai/calendar-vim",
     },
     config = function()
-
-      require('telekasten').setup({
+      require("telekasten").setup({
+        media_previewer = "chafa-previewer",
         home = vim.fn.expand("~/git/" .. io.popen("whoami"):read("*a"):gsub("\n", "") .. "/zet"),
         templates = vim.fn.expand("~/git/" .. io.popen("whoami"):read("*a"):gsub("\n", "") .. "/zet/templates"),
         new_note_filename = "uuid-title",
-        filename_space_subst = '_',
-        uuid_type = '%Y-%m-%d-%H%M',
-        template_new_note = vim.fn.expand("~/git/" .. io.popen("whoami"):read("*a"):gsub("\n", "") .. "/zet/templates/base_note.md"),
-        template_new_daily = vim.fn.expand("~/git/" .. io.popen("whoami"):read("*a"):gsub("\n", "") .. "/zet/templates/daily.md"),
-        template_new_weekly = vim.fn.expand("~/git/" .. io.popen("whoami"):read("*a"):gsub("\n", "") .. "/zet/templates/weekly.md"),
+        filename_space_subst = "_",
+        uuid_type = "%Y-%m-%d-%H%M",
+        template_new_note = vim.fn.expand(
+          "~/git/" .. io.popen("whoami"):read("*a"):gsub("\n", "") .. "/zet/templates/base_note.md"
+        ),
+        template_new_daily = vim.fn.expand(
+          "~/git/" .. io.popen("whoami"):read("*a"):gsub("\n", "") .. "/zet/templates/daily.md"
+        ),
+        template_new_weekly = vim.fn.expand(
+          "~/git/" .. io.popen("whoami"):read("*a"):gsub("\n", "") .. "/zet/templates/weekly.md"
+        ),
       })
 
       -- Launch panel if nothing is typed after <leader>z
@@ -31,18 +37,18 @@ return {
       vim.keymap.set("n", "<leader>zc", "<cmd>Telekasten show_calendar<CR>")
       vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")
       vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_note<CR>")
-    end
+    end,
   },
   {
-    'navicore/nvim-reminders',
+    "navicore/nvim-reminders",
     config = function()
       -- require('reminders').setup({
       --   paths = { '~/git/navicore/zet' },
       --   --recursive_scan = true,
       -- })
-      require('reminders').setup()
+      require("reminders").setup()
       vim.keymap.set("n", "<leader>zr", "<cmd>ReminderScan<CR>")
       vim.keymap.set("n", "<leader>zre", "<cmd>ReminderEdit<CR>")
-    end
+    end,
   },
 }
