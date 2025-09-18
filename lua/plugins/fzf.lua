7136b3d4e4aa6dbab6b968695cc55aea1f7ea8ac
Ed Sweeney <ed@onextent.com>
2025-09-18 15:32:38 -0700

    non-lazy Rg and zim

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,6 +1,25 @@
 return {
+  {
+    "junegunn/fzf",
+    build = ":call fzf#install()",
+    lazy = false, -- Load immediately for commands
+  },
+  {
+    "junegunn/fzf.vim",
+    lazy = false, -- Load immediately for Rg command
+    dependencies = {
+      "junegunn/fzf",
+      "BurntSushi/ripgrep",
+    },
+    config = function()
+      -- Ensure Rg command is available
+      vim.cmd([[
+        command! -bang -nargs=* Rg
+          \ call fzf#vim#grep(
+          \   'rg --column --line-number --no-heading --color=always --smart-case -- '.shellescape(<q-args>), 1,
+          \   fzf#vim#with_preview(), <bang>0)
+      ]])
+    end,
+  },
   "nvim-telescope/telescope-fzf-native.nvim",
-  "junegunn/fzf.vim",
-  "BurntSushi/ripgrep",
-  { "junegunn/fzf", build = ":call fzf#install()" },
 }
--- lua/plugins/zim.lua
+++ lua/plugins/zim.lua
@@ -1,17 +1,11 @@
 return {
-  -- {
-  --   "navicore/zim-sequencer",
-  --   dependencies = { "nvim-lua/plenary.nvim" },
-  --   build = "cd engine && cargo build --release",
-  --   config = function()
-  --     require("sequencer").setup()
-  --   end,
-  -- },
   {
     "voldikss/vim-floaterm",
+    lazy = false, -- Load immediately as dependency for zim-studio
   },
   {
     "navicore/zim-studio-nvim",
+    lazy = false, -- Load immediately for Zim commands
     dependencies = {
       "voldikss/vim-floaterm", -- or 'akinsho/toggleterm.nvim'
     },
@@ -19,4 +13,13 @@ return {
       require("zim-studio").setup()
     end,
   },
+  -- {
+  --   "navicore/zim-sequencer",
+  --   lazy = false,
+  --   dependencies = { "nvim-lua/plenary.nvim" },
+  --   build = "cd engine && cargo build --release",
+  --   config = function()
+  --     require("sequencer").setup()
+  --   end,
+  -- },
 }
