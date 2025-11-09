56aa92d9aa517f7838be22c42b419fd3b1fbfbf9
Ed Sweeney <ed@onextent.com>
2025-11-09 08:42:29 -0800

    no flash or leap

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,9 +1,6 @@
 return {
-  -- disable trouble
-  --{ "folke/trouble.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "afamadriz/friendly-snippets", enabled = false },
   { "nvim-lualine/lualine.nvim", enabled = false },
-  --{ "folke/noice.nvim", enabled = false },
 }
