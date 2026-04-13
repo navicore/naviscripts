17d922765d83d31c5f31ce1b9161296c79afb89c
Ed Sweeney <ed@onextent.com>
2026-04-13 13:51:37 -0700

    nochat

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -15,44 +15,4 @@ return {
       })
     end,
   },
-
-  -- CopilotChat
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    event = "VeryLazy",
-    branch = "main",
-    dependencies = {
-      { "nvim-lua/plenary.nvim", branch = "master" },
-      { "MeanderingProgrammer/render-markdown.nvim" },
-    },
-    build = "make tiktoken",
-    config = function()
-      require("CopilotChat").setup({
-        highlight_headers = false,
-        separator = "---",
-        error_header = "> [!ERROR] Error",
-      })
-      vim.keymap.set("n", "<leader>cc", "<CMD>CopilotChatToggle<CR>", { desc = "Open Copilot Chat" })
-    end,
-    keys = {
-      {
-        "<leader>ccq",
-        function()
-          local input = vim.fn.input("Quick Chat: ")
-          if input ~= "" then
-            require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
-          end
-        end,
-        desc = "CopilotChat - Quick chat",
-      },
-      {
-        "<leader>ccp",
-        function()
-          local actions = require("CopilotChat.actions")
-          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
-        end,
-        desc = "CopilotChat - Prompt actions",
-      },
-    },
-  },
 }
